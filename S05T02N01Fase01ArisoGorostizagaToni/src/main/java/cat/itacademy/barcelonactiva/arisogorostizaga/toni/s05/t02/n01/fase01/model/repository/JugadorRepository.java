9a7ff2291fd645eea430f43ed0a977e1605c6151
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-07-14 11:44:12 +0200

    JugadorRepository

---
+++ S05T02N01Fase01ArisoGorostizagaToni/src/main/java/cat/itacademy/barcelonactiva/arisogorostizaga/toni/s05/t02/n01/fase01/model/repository/JugadorRepository.java
@@ -0,0 +1,9 @@
+package cat.itacademy.barcelonactiva.arisogorostizaga.toni.s05.t02.n01.fase01.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import cat.itacademy.barcelonactiva.arisogorostizaga.toni.s05.t02.n01.fase01.model.domain.Jugador;
+
+public interface JugadorRepository extends JpaRepository<Jugador, Integer>{
+
+}
