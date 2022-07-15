8dd8dd77ecff95c0715cedb5e151b7465dc01553
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-07-15 04:21:06 +0200

    Joc Service

---
+++ S05T02N01Fase01ArisoGorostizagaToni/src/main/java/cat/itacademy/barcelonactiva/arisogorostizaga/toni/s05/t02/n01/fase01/model/services/JocService.java
@@ -0,0 +1,10 @@
+package cat.itacademy.barcelonactiva.arisogorostizaga.toni.s05.t02.n01.fase01.model.services;
+
+import cat.itacademy.barcelonactiva.arisogorostizaga.toni.s05.t02.n01.fase01.model.domain.Joc;
+import cat.itacademy.barcelonactiva.arisogorostizaga.toni.s05.t02.n01.fase01.model.domain.Jugador;
+
+public interface JocService {
+	
+	public Jugador crearJoc(int idJugador, Joc joc);
+
+}
---
+++ S05T02N01Fase01ArisoGorostizagaToni/src/main/java/cat/itacademy/barcelonactiva/arisogorostizaga/toni/s05/t02/n01/fase01/model/services/JocServiceImpl.java
@@ -0,0 +1,5 @@
+package cat.itacademy.barcelonactiva.arisogorostizaga.toni.s05.t02.n01.fase01.model.services;
+
+public class JocServiceImpl {
+
+}
