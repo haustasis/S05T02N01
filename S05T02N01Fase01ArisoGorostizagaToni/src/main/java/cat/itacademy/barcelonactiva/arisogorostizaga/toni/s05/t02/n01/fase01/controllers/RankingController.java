718da6e0268fabadb3a5d85f83dbd16d61d7e505
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-07-18 02:35:37 +0200

    ranking controller

--- S05T02N01Fase01ArisoGorostizagaToni/src/main/java/cat/itacademy/barcelonactiva/arisogorostizaga/toni/s05/t02/n01/fase01/controllers/RankingController.java
+++ S05T02N01Fase01ArisoGorostizagaToni/src/main/java/cat/itacademy/barcelonactiva/arisogorostizaga/toni/s05/t02/n01/fase01/controllers/RankingController.java
@@ -1,16 +1,38 @@
 package cat.itacademy.barcelonactiva.arisogorostizaga.toni.s05.t02.n01.fase01.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import cat.itacademy.barcelonactiva.arisogorostizaga.toni.s05.t02.n01.fase01.model.services.JugadorService;
+import cat.itacademy.barcelonactiva.arisogorostizaga.toni.s05.t02.n01.fase01.model.services.RankingService;
 
 @RestController
 @RequestMapping("/players/ranking")
 public class RankingController {
 	
 	@Autowired
-	private JugadorService jugadorService;
+	private RankingService rankingService;
+	
+	@GetMapping
+	public ResponseEntity<?> llistaJugadorsGuanyatPercent() throws Exception {
+		
+		return ResponseEntity.status(HttpStatus.OK).body(rankingService.llistaJugadorsGuanyatPercent());
+	}
+	
+	@GetMapping("/winner")
+	public ResponseEntity<?> millorJugadorGuanyatPercent() throws Exception{
 
+		return ResponseEntity.status(HttpStatus.OK).body(rankingService.millorJugadorGuanyatPercent());
+	}
+	
+	@GetMapping("/loser")
+	public ResponseEntity<?> pitjorJugadorGuanyatPercent() throws Exception{
+		
+		return ResponseEntity.status(HttpStatus.OK).body(rankingService.pitjorJugadorGuanyatPercent());
+	}
+	
+	
 }
