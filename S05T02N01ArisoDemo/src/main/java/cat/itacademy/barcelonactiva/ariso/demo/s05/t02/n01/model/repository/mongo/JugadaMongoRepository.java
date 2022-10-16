1077e8067c49d4f848afe4183d07b41b43b5abb8
haustasis <toni.ariso@gmail.com>
2022-10-16 05:49:32 +0200

    remove mongo classes and dependencies after bug

--- S05T02N01ArisoDemo/src/main/java/cat/itacademy/barcelonactiva/ariso/demo/s05/t02/n01/model/repository/mongo/JugadaMongoRepository.java
+++
@@ -1,9 +0,0 @@
-package cat.itacademy.barcelonactiva.ariso.demo.s05.t02.n01.model.repository.mongo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import cat.itacademy.barcelonactiva.ariso.demo.s05.t02.n01.model.domain.mongo.JugadaMongo;
-
-public interface JugadaMongoRepository extends MongoRepository<JugadaMongo, Long> {
-
-}
--- S05T02N01ArisoDemo/src/main/java/cat/itacademy/barcelonactiva/ariso/demo/s05/t02/n01/model/services/mongo/JugadorMongoServiceImpl.java
+++
@@ -1,5 +0,0 @@
-package cat.itacademy.barcelonactiva.ariso.demo.s05.t02.n01.model.services.mongo;
-
-public class JugadorMongoServiceImpl {
-
-}
